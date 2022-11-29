7e1642f719fd9c47870c82b6e6c65b8da1785e84
Matthew Madden <matthewkayin@gmail.com>
2022-11-29 15:05:29 -0800

    Add spinning items
    Signed-off-by: Matthew Madden <matthewkayin@gmail.com>

--- items/item_drop.gd
+++ items/item_drop.gd
@@ -1,17 +1,23 @@
 extends Node2D
 
 onready var sprite = $sprite
+onready var timer = $timer
 
 var item: int
 var coordinate: Vector2
 
 func _ready():
-    pass 
+    timer.connect("timeout", self, "_on_timeout")
 
 func spawn(at_coordinate: Vector2, with_item: int):
     coordinate = at_coordinate
     position = coordinate * 32
     item = with_item
     sprite.texture = Items.DATA[item].texture
+    sprite.frame = 0 
+    timer.start(0.2)
 
     add_to_group("items")
+
+func _on_timeout():
+    sprite.frame = (sprite.frame + 1) % sprite.hframes
